75e30f303aa75a220e5137f24ff5bb67883ac39e
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2017-06-15 13:05:24 +0000

    fixed weird bash/screen resetting some parts of the modules

--- profile.d/z-20-lmod.sh
+++ profile.d/z-20-lmod.sh
@@ -1,7 +1,5 @@
+export MODULESHOME=/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/lmod/lmod
 source /cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/lmod/lmod/init/profile
-#export BASH_ENV=/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/lmod/lmod/init/bash
-export MODULEPATH=
-module use /cvmfs/soft.computecanada.ca/custom/modules
 export LMOD_PACKAGE_PATH=/cvmfs/soft.computecanada.ca/config/lmod/
 export MODULERCFILE=$LMOD_PACKAGE_PATH/modulerc
 export LMOD_AVAIL_STYLE=grouped:system
@@ -12,15 +10,18 @@ if [[ -z "$RSNT_ARCH" ]]; then
 	export RSNT_ARCH="avx2"
 fi
 
-if [[ -d $HOME/modulefiles ]]; then
-	module -q use --priority 100 $HOME/modulefiles
-fi
 
 if [[ -z "$__Init_Default_Modules" || -z "$LD_LIBRARY_PATH" ]]; then
+	export MODULEPATH=
+	module use /cvmfs/soft.computecanada.ca/custom/modules
 	__Init_Default_Modules=1; export __Init_Default_Modules;
 	export LMOD_SYSTEM_DEFAULT_MODULES="StdEnv/2016.4"
 	module --initial_load restore
 else
 	module refresh
+fi
+
+if [[ -d $HOME/modulefiles ]]; then
+	module -q use --priority 100 $HOME/modulefiles
 fi
 
