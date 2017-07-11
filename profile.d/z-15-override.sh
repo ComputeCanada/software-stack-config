43a5d6a00b4369b6928ecc53a6ab6001dbb36f72
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-07-11 15:33:39 +0000

    Various fixes: * remove obsolete /usr/nfsshare/slurm/bin from $PATH * add /opt/software/slurm/lib /opt/software/lib to $LD_LIBRARY_PATH   mainly to access libpmi * disable Mellanox IB on cedar which has OmniPath, to avoid error   noise

--- profile.d/z-15-override.sh
+++ profile.d/z-15-override.sh
@@ -1,9 +1,18 @@
 if [[ "$CC_CLUSTER" == "graham" || "$CC_CLUSTER" == "cedar" ]]; 
 then
-	for d in /opt/software/slurm/bin /usr/nfsshare/slurm/bin /opt/puppetlabs/puppet/bin; do
+	for d in /opt/software/slurm/bin /opt/software/bin ; do
 		if [[ -d "$d" ]]; then
 			export PATH=$d:$PATH
 		fi
 	done
+        for d in /opt/software/slurm/lib /opt/software/lib ; do
+		if [[ -d "$d" ]]; then
+			export LD_LIBRARY_PATH=$d${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+		fi
+	done
+	if [[ "$CC_CLUSTER" == "cedar" ]];
+	then
+		export OMPI_MCA_mtl=^mxm
+		export OMPI_MCA_pml=^yalla
+	fi
 fi
-
