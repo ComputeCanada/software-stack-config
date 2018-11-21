47057b5bd91b7a34190716800c9c901b09f0c807
root <root@build-node.computecanada.ca>
2018-11-21 18:03:17 +0000

    disabled easybuild autocomplete

--- profile.d/z-30-easybuild.sh
+++ profile.d/z-30-easybuild.sh
@@ -1,3 +1,3 @@
-source /cvmfs/soft.computecanada.ca/easybuild/framework/minimal_bash_completion.bash
-source /cvmfs/soft.computecanada.ca/easybuild/framework/optcomplete.bash
-source /cvmfs/soft.computecanada.ca/easybuild/framework/eb_bash_completion.bash
+#source /cvmfs/soft.computecanada.ca/easybuild/framework/minimal_bash_completion.bash
+#source /cvmfs/soft.computecanada.ca/easybuild/framework/optcomplete.bash
+#source /cvmfs/soft.computecanada.ca/easybuild/framework/eb_bash_completion.bash
