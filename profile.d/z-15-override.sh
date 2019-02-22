b351a88541b451d70a8553521ef21d2135274ec0
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2019-02-22 10:24:27 -0500

    Update z-15-override.sh

--- profile.d/z-15-override.sh
+++ profile.d/z-15-override.sh
@@ -4,8 +4,8 @@ for d in (/opt/software/slurm/bin /opt/software/bin /opt/slurm/bin) ; do
 		export PATH=$d:$PATH
 	fi
 done
-for d in (/opt/software/slurm/lib /opt/software/slurm/lib64 /opt/software/lib /opt/software/lib64 /opt/slurm/lib64) ; do
-	if [[ -d "$d" ]]; then
+for d in (/opt/software/slurm/lib /opt/software/slurm/lib64 /opt/software/slurm/lib64 /opt/slurm/lib64) ; do
+	if [[ -f "$d/libslurm.so" ]]; then
 		export LD_LIBRARY_PATH=$d${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 	fi
 done
