14ef30548a2fff3a573fdb430870f4cb04ff9a2c
Bart Oldeman <bart.oldeman@calculquebec.ca>
2019-04-01 13:08:40 +0000

    Recover CC_CLUSTER from /etc/environment if unset and possible.

--- profile.d/z-000-init.csh
+++ profile.d/z-000-init.csh
@@ -1,3 +1,7 @@
+if ( ! $?CC_CLUSTER && -r /etc/environment ) then
+	# try to recover from /etc/environment (not used by slurm)
+	setenv CC_CLUSTER `grep ^CC_CLUSTER /etc/environment | cut -d= -f2`
+endif
 if ( ! $?CC_CLUSTER ) then
 	setenv CC_CLUSTER "computecanada"
 endif
--- profile.d/z-000-init.sh
+++ profile.d/z-000-init.sh
@@ -1,3 +1,7 @@
+if [[ -z "$CC_CLUSTER" && -r /etc/environment ]]; then
+	# try to recover from /etc/environment (not used by slurm)
+	export CC_CLUSTER=$(grep ^CC_CLUSTER /etc/environment | cut -d= -f2)
+fi
 if [[ -z "$CC_CLUSTER" ]]; then
 	export CC_CLUSTER="computecanada"
 fi
