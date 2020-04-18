3618856fce5e0c1154bf4fa432a62a6300c4ceba
Bart Oldeman <bart.oldeman@calculquebec.ca>
2020-04-18 00:12:20 +0000

    Adapt Intel MPI config to 2019.7

--- lmod/intelmpi_custom.lua
+++ lmod/intelmpi_custom.lua
@@ -3,14 +3,20 @@ local posix = require "posix"
 
 local slurmpaths = { "/opt/software/slurm/lib", "/opt/software/slurm/lib64",
                      "/opt/slurm/lib64" }
-for i,v in ipairs(slurmpaths) do
-	if posix.stat(pathJoin(v,"libpmi.so"),"type") == "link" then
-		setenv("I_MPI_PMI_LIBRARY", pathJoin(v,"libpmi.so"))
-		break
+
+if impiv == "2019.7" then
+	setenv("I_MPI_PMI_LIBRARY", "libpmi2.so")
+	setenv("I_MPI_HYDRA_TOPOLIB", "ipl")
+else
+	for i,v in ipairs(slurmpaths) do
+		if posix.stat(pathJoin(v,"libpmi.so"),"type") == "link" then
+			setenv("I_MPI_PMI_LIBRARY", pathJoin(v,"libpmi.so"))
+			break
+		end
 	end
 end
 
-if impiv ~= "2019.3" then
+if string.sub(impiv,1,4) ~= "2019" then
 	if os.getenv("RSNT_INTERCONNECT") == "omnipath" then
 		setenv("I_MPI_FABRICS_LIST", "ofi,tmi,dapl,tcp,ofa")
 	else
