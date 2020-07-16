f2fe916cfbbb744afc7b9426c70949bc38d7e5e8
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-07-16 15:18:20 +0000

    added missing local posix = require posix

--- lmod/SitePackage.lua
+++ lmod/SitePackage.lua
@@ -177,6 +177,7 @@ end
 sandbox_registration{ get_interconnect = get_interconnect }
 function get_installed_cuda_driver_version()
 	local lfs       = require("lfs")
+	local posix = require "posix"
 	if posix.stat("/usr/lib64/nvidia","type") == 'directory' then
 		for f in lfs.dir('/usr/lib64/nvidia') do
 			local name = f:match("^(.+%.so).+$")
