6ec4efe893d9e2cf7149fc2f04bb7414ef2026bc
Bart Oldeman <bart.oldeman@calculquebec.ca>
2021-01-04 16:58:42 +0000

    Commit Intel MPI updates dealing with wrong Slurm cpu bindings

--- lmod/intelmpi_custom.lua
+++ lmod/intelmpi_custom.lua
@@ -3,16 +3,31 @@ local posix = require "posix"
 
 local slurmpaths = { "/opt/software/slurm/lib", "/opt/software/slurm/lib64",
                      "/opt/slurm/lib64" }
+local slurmpath = nil
+
+for i,v in ipairs(slurmpaths) do
+	if posix.stat(pathJoin(v,"libslurm.so"),"type") == "link" then
+		slurmpath = v
+		break
+	end
+end
+
+if slurmpath then
+	if posix.stat(pathJoin(slurmpath,"libslurm.so.36"),"type") == "link" then
+		-- we need to use --whole for Slurm 20.11 which ships with .so.36
+		-- older Slurms use .so.35 or older
+		setenv("I_MPI_HYDRA_BOOTSTRAP_EXEC_EXTRA_ARGS", "--whole")
+	else
+		setenv("I_MPI_HYDRA_BOOTSTRAP_EXEC_EXTRA_ARGS", "--cpu-bind=none")
+	end
+end
 
 if impiv == "2019.7" then
 	setenv("I_MPI_PMI_LIBRARY", "libpmi2.so")
 	setenv("I_MPI_HYDRA_TOPOLIB", "ipl")
 else
-	for i,v in ipairs(slurmpaths) do
-		if posix.stat(pathJoin(v,"libpmi.so"),"type") == "link" then
-			setenv("I_MPI_PMI_LIBRARY", pathJoin(v,"libpmi.so"))
-			break
-		end
+	if posix.stat(pathJoin(slurmpath,"libpmi.so"),"type") == "link" then
+		setenv("I_MPI_PMI_LIBRARY", pathJoin(slurmpath,"libpmi.so"))
 	end
 end
 
