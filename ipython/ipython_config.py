62da27ec8be90b948ebb6bfd7a576eafaa1daf7c
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2023-08-31 14:37:37 +0000

    move ipython history to memory instead of filesystem

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,2 +1,5 @@
 c = get_config()  #noqa
-c.HistoryAccessor.enabled = False
+# Move IPython history to in-memory sqlite instead of on-disk.
+# This avoids kernel hanging issues with network filesystem like Lustre and NFS.
+c.HistoryAccessor.hist_file = ":memory:"
+#c.HistoryAccessor.enabled = False
